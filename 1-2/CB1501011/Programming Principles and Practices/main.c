ed7afb4cbc59e335dfd52af3df4bd45dc7522ea6
HIX_21A151921-004123 <rnjsalsrb6666@naver.com>
2023-11-09 18:38:02 +0900

    231109

--- 1-2/CB1501011/Programming Principles and Practices/main.c
+++ 1-2/CB1501011/Programming Principles and Practices/main.c
@@ -1,42 +1,19 @@
-#include <math.h>
 #include <stdio.h>
+#include <string.h>
 
-/**
-* @brief : Find an approximation to the square root of a non-negative number
-using the Newton's method.
-* @return: An approximation to the square root. The square of it should be in
-the range [x-epsilon, x+epsilon]. -1 if it can't find an answer.
-* @param : x - target number, epsilon - the allowed difference
-*/
-double findroot_newton(double x, double epsilon) {
-  double ans = x / 2.0;
-  int numGuesses = 0;
-
-  double diff = fabs(ans * ans - x);
-
-  while (diff >= epsilon) {
-    ans = (ans * ans + x) / (2 * ans);
-
-    diff = fabs(ans * ans - x);
-    numGuesses = numGuesses + 1;
-  }
-
-  printf("# of Guesses = %d   ", numGuesses);
-
-  return ans;
-}
 
 int main(void) {
-  double x;
-  double epsilon = 0.001;
-  for (int i = -10000000; i < 10000000; i++) {
-    x = i;
+  char str1[256] = "Hello World !!!\n Good Day !!!";
+  char str2[256] = "Hello PNU CSE !!!";
+  char str3[256];
 
-    if (x > 0) {
-      printf("[  NEWTON  ] sqrt(%g) ~= %g\n", x,
-             findroot_newton(x, epsilon));
-    }
-  }
+  printf("strlen(str1) = %d, strlen(str2) = %d\n", strlen(str1),
+         strlen(str2));
+
+  printf("strcpy(str3,str1) = %s\n", strcpy(str3, str1));
+
+  printf("strcmp(str1,str3) = %d, strcmp(str2,str3) = %d",
+         strcmp(str1, str3), strcmp(str2, str3));
 
   return 0;
 }
