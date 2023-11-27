e1d2a746320f383e9d731353d3b4b05840b58be0
HIX_21A151921-004123 <rnjsalsrb6666@naver.com>
2023-11-27 16:22:52 +0900

    auto commit

--- baekjoon.c
+++ baekjoon.c
@@ -1,22 +1,37 @@
-
+#include <math.h>
 #include <stdio.h>
-typedef struct student {
-  int id;
-  char *pname;
-  double points;
-} STUD;
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+  return (*(unsigned int *)a - *(unsigned int *)b);
+}
+
+unsigned int calc_intercept(unsigned int n) { return round((float)n * 0.15); }
+
+void scan_nums(unsigned int *nums, unsigned int n) {
+  while (n--) scanf("%d", nums++);
+}
+
+int calc_sum(unsigned int *nums, unsigned int n, unsigned int intercept) {
+  unsigned int sum = 0;
+  for (unsigned int *i = nums + intercept; i < nums + n - intercept; i++)
+    sum += *i;
+  return sum;
+}
 
 int main(void) {
-  int i;  // index
-  STUD pnucse[] = {{1, "Choi", 9.9}, {2, "Park", 0.1}, {3, "Kim", 5.0},
-                   {4, "Lee", 3.0},  {5, "Moon", 9.5}, {6, "Kang", 7.0},
-                   {7, "Jeon", 0.9}, {-1, NULL, 0}};
+  unsigned int n;
+  scanf("%d", &n);
 
-  STUD *ptr = pnucse;
+  unsigned int intercept = calc_intercept(n);
+  unsigned int difficulty[n];
+  scan_nums(difficulty, n);
 
-  for (; ptr->id >= 0; ptr++) {
-    printf("[%d:%s] = %lf\n", ptr->id, ptr->pname, ptr->points);
-  }
+  qsort(difficulty, n, sizeof(unsigned int), compare);
+
+  unsigned int sum = calc_sum(difficulty, n, intercept);
+
+  printf("%d", (int)round((float)sum / (n - 2 * intercept)));
 
   return 0;
 }
