0c7f88b524aae47a246fdae2e220642f9cc152a9
HIX_21A151921-004123 <rnjsalsrb6666@naver.com>
2023-11-28 21:31:37 +0900

    auto commit

--- baekjoon.c
+++ baekjoon.c
@@ -1,37 +1,10 @@
-#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
-
-int compare(const void *a, const void *b) {
-  return (*(unsigned int *)a - *(unsigned int *)b);
-}
-
-unsigned int calc_intercept(unsigned int n) { return round((float)n * 0.15); }
-
-void scan_nums(unsigned int *nums, unsigned int n) {
-  while (n--) scanf("%d", nums++);
-}
-
-int calc_sum(unsigned int *nums, unsigned int n, unsigned int intercept) {
-  unsigned int sum = 0;
-  for (unsigned int *i = nums + intercept; i < nums + n - intercept; i++)
-    sum += *i;
-  return sum;
-}
 
 int main(void) {
-  unsigned int n;
-  scanf("%d", &n);
+  int numCases, numDocs, targetDoc;
+  scanf("%d", &numCases);
 
-  unsigned int intercept = calc_intercept(n);
-  unsigned int difficulty[n];
-  scan_nums(difficulty, n);
-
-  qsort(difficulty, n, sizeof(unsigned int), compare);
-
-  unsigned int sum = calc_sum(difficulty, n, intercept);
-
-  printf("%d", (int)round((float)sum / (n - 2 * intercept)));
+while(numCases--) {}
 
   return 0;
 }
--- copilot.c
+++ copilot.c
@@ -1,21 +1,58 @@
+#include <math.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 typedef struct {
-  int x;
-  int y;
-} Pos;
+  unsigned int num;
+  bool isPrime;
+} Prime;
 
-int qcmp(const void *a, const void *b) {
-  const Pos *foo = a, *bar = b;
-  return (foo->y == bar->y) ? (foo->x - bar->x) : (foo->y - bar->y);
+void gen_list(Prime* prime, unsigned int start, unsigned int end) {
+  unsigned int rangeWidth = end - start + 1;
+  for (unsigned int i = 0; i < rangeWidth; i++) {
+    prime[i].num = start + i;
+    prime[i].isPrime = true;
+  }
 }
 
-int main(void) {
-  int len;
-  scanf("%d", &len);
-  Pos pos[len];
-  for (Pos *i = pos; i < pos + len; i++) scanf("%d %d", &i->x, &i->y);
-  qsort(pos, len, sizeof(Pos), qcmp);
-  for (Pos *i = pos; i < pos + len; i++) printf("%d %d\n", i->x, i->y);
+void check_prime(Prime* prime, unsigned int start, unsigned int end) {
+  unsigned int rangeWidth = end - start + 1;
+  for (unsigned int i = 0; i < rangeWidth; i++) {
+    if (prime[i].num == 1) {
+      prime[i].isPrime = false;
+      continue;
+    }
+    for (unsigned int j = 2; j <= sqrt(prime[i].num); j++) {
+      if (prime[i].num % j == 0) {
+        prime[i].isPrime = false;
+        break;
+      }
+    }
+  }
+}
+
+Prime* create_and_initialize_primes(unsigned int start, unsigned int end) {
+  Prime* primes = malloc((end - start + 1) * sizeof(Prime));
+  gen_list(primes, start, end);
+  check_prime(primes, start, end);
+  return primes;
+}
+
+void print_prime(Prime* prime, unsigned int start, unsigned int end) {
+  unsigned int rangeWidth = end - start + 1;
+  for (unsigned int i = 0; i < rangeWidth; i++) {
+    if (prime[i].isPrime) {
+      printf("%d\n", prime[i].num);
+    }
+  }
+}
+
+int main() {
+  unsigned int start, end;
+  scanf("%d %d", &start, &end);
+
+  Prime* primes = create_and_initialize_primes(start, end);
+  print_prime(primes, start, end);
+
+  free(primes);
 }
